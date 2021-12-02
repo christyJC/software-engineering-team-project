128ab1578c061930fed0a70ed8d1031aa8c9a04e
garciam3 <garciam3@mail.gvsu.edu>
2021-12-01 21:49:59 -0500

    Added levels to switch between

---
+++ Team Project/source/Levels/Portal.gd
@@ -0,0 +1,17 @@
+tool
+
+extends Area2D
+
+export(String, FILE) var next_scene_path = ""
+
+
+func _get_configuration_warning() -> String:
+	if next_scene_path == "":
+		return "next_scene_path must be set for the portal to work"
+	else: 
+		return ""
+
+
+func _on_Portal_body_entered(body):
+	if get_tree().change_scene(next_scene_path) != OK:
+		print("Error: Unavailable Scene!")
